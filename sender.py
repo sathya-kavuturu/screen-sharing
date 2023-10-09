eaf27affeff9e57f90f1dff1b8bd58d9eefb2985
sathya kavuturu <69446807+sathya-kavuturu@users.noreply.github.com>
2023-10-09 16:31:21 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,7 +1,7 @@
 #sender.py
 from vidstream import ScreenShareClient
 import threading
-
+# Give IP address of target laptop to which screen sharing is to be shared
 sender = ScreenShareClient('192.168.150.29', 9999)
 
 t = threading.Thread(target=sender.start_stream) 
